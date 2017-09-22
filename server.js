b91c1a8125f159cbb7ca1cd4e2df54013f995879
Sergey Boychuk <sergeboychuk@gmail.com>
2017-09-22 01:05:09 -0400

    Added ejs

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/' , (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/about' , (req, res) => {
+  res.render('pages/about');
+});
+
+
+app.listen(8080);
+console.log('8080 is the magic port');
