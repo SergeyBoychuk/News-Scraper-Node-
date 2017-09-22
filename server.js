652eb69c3af9ac434dc6475ca17a70a2854f37b2
Sergey Boychuk <sergeboychuk@gmail.com>
2017-09-22 14:23:46 -0400

    Got titles to display with summary underneath

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/' , (req, res) => {
-  let titles = [
-    data.title
+  let dataDisplayed = [
+    data.title,
+    data.summary
   ]
+
   res.render('pages/index', {
-    titles: titles
+    dataDisplayed
   });
-  
+
 });
+
+
 
 app.get('/about' , (req, res) => {
   res.render('pages/about');
@@ -28,27 +32,27 @@ console.log('8080 is the magic port');
 let url = 'http://www.mysuncoast.com/news/';
 
 let data = {
-    title: [],
-    summary: []
+  title: [],
+  summary: []
 }
 
-request(url, (err,resp,body) => {
-    let $ = cheerio.load(body)
-        
-        
-        
-        $('.panel').each((i, elem) => {
-            try {
-                data.title[i] = $('.card-headline h3 a').get(i).children[0].data.replace(/(\r\n|\n|\r)/gm,"").trim();
-                data.summary[i] = $('.card-lead p').get(i).children[0].data;
-            } catch(e) {
 
-            }
-            
-        })
-        console.log(data.title);
-   
-    
-    
-        fs.writeFileSync('./data.json', JSON.stringify(data , null , 2), 'utf-8');
+
+request(url, (err,resp,body) => {
+  let $ = cheerio.load(body)
+
+  $('.panel').each((i, elem) => {
+    try {
+      data.title[i] = $('.card-headline h3 a').get(i).children[0].data.replace(/(\r\n|\n|\r)/gm,"").trim();
+      data.summary[i] = $('.card-lead p').get(i).children[0].data;
+    } catch(e) {
+
+    }
+  })
+  console.log(data.title);
+
+
+
+  fs.writeFileSync('./data.json', JSON.stringify(data , null , 2), 'utf-8');
 });
+
