8b9c1094c4b2be597420de005a29e71f61a60c02
Sergey Boychuk <sergeboychuk@gmail.com>
2017-09-22 01:09:05 -0400

    Cleaned up folder structure

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const http = require('http');
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -11,6 +16,33 @@ app.get('/about' , (req, res) => {
   res.render('pages/about');
 });
 
-
 app.listen(8080);
 console.log('8080 is the magic port');
+
+let url = 'http://www.mysuncoast.com/news/';
+
+let data = {
+    title: [],
+    summary: []
+}
+
+request(url, (err,resp,body) => {
+    let $ = cheerio.load(body)
+        
+        
+        
+        $('.panel').each((i, elem) => {
+            try {
+                data.title[i] = $('.card-headline h3 a').get(i).children[0].data.replace(/(\r\n|\n|\r)/gm,"").trim();
+                data.summary[i] = $('.card-lead p').get(i).children[0].data;
+            } catch(e) {
+
+            }
+            
+        })
+        console.log(data.title);
+   
+    
+    
+        fs.writeFileSync('./data.json', JSON.stringify(data , null , 2), 'utf-8');
+});
